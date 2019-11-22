b79c6f318351d71d3f0c06b6e39a38fda7d965df
Tord Kriznik Sørensen <sorensen@stcorp.no>
2019-11-22 21:32:17 +0100

    Add installation using dotbot

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
 # dotfiles
 
-My dotfiles
-
-
-TODO:
-- Script for setting up
-    - Create correct folders
-    - Symlinking
-    - Install plugin dependencies
-    - Set up vim-plug and antigen and install zsh and nvim plugins
+install by calling ./install
--- install_old.sh
+++ install_old.sh
@@ -1,3 +1,5 @@
+# TODO: tidy up this old install stuff
+# TODO: Is any of this useful for new setup?
 
 # NOTE: This is currently only a collection of steps and will be sewed togheter when time
 # Should install both bash/zsh settings and nvim settings on a new computer
@@ -5,31 +7,22 @@
 
 
 
-# Autoinstall vim-plug
-# TODO: Make this work
-#if empty(glob('~/.nvim/autoload/plug.vim')) ; then
-  #silent !curl -fLo ~/.nvim/autoload/plug.vim --create-dirs
-    #\ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-  #autocmd VimEnter * PlugInstall
-#endif
-
-
 # TODO: Fix python setup given a virtual environment
 # Setting up python
 # https://github.com/deoplete-plugins/deoplete-jedi/wiki/Setting-up-Python-for-Neovim
-pip install --user neovim
+# pip install --user neovim
 
 
 # deoplete install
-pip install --user pynvim
-pip install --user --upgrade pynvim
+# pip install --user pynvim
+# pip install --user --upgrade pynvim
 
 # jedi install
-pip install jedi
+# pip install jedi
 
 # black install
-pip install black
-pip install yapf
+# pip install black
+# pip install yapf
 
 
 # fasd
@@ -38,3 +31,4 @@ pip install yapf
 
 # ag install 
 # apt-get install silversearcher-ag
+
