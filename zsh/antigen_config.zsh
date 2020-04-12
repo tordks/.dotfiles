cf560a709dfa92472a590eef6dd5928a0cc7e0ab
Tord Kriznik Sørensen <sorensen@stcorp.no>
2020-04-12 11:32:16 +0200

    Update zsh and tmux installs

--- zsh/antigen_config.zsh
+++ zsh/antigen_config.zsh
@@ -8,7 +8,7 @@ antigen use oh-my-zsh
 antigen bundle git
 antigen bundle vi-mode
 antigen bundle zsh-autosuggestions
-#antigen bundle heroku
+antigen bundle heroku
 #antigen bundle pip
 #antigen bundle lein
 #antigen bundle command-not-found
@@ -17,29 +17,38 @@ antigen bundle zsh-autosuggestions
 antigen bundle djui/alias-tips # gives tips if alias for a command exist
 antigen bundle zdharma/fast-syntax-highlighting #syntax highlighting in shell
 
-
 antigen bundle junegunn/fzf # fuzzy file finder
-antigen bundle andrewferrier/fzf-z # combines fzf and z/fasd/autojump
+#antigen bundle andrewferrier/fzf-z # combines fzf and z/fasd/autojump
 antigen bundle hschne/fzf-git # enables fzf fuzzy search with git
+#antigen bundle wookayin/fzf-fasd # tab completion of z with fzf
 
 antigen bundle clvv/fasd # fuzzy file search inspired by z, autojump, v
-source $ZSH/plugins/fasd/fasd.plugin.zsh
-alias a='fasd -a'        # any
-alias s='fasd -si'       # show / search / select
-alias d='fasd -d'        # directory
-alias f='fasd -f'        # file
-alias sd='fasd -sid'     # interactive directory selection
-alias sf='fasd -sif'     # interactive file selection
-alias z='fasd_cd -d'     # cd, same functionality as j in autojump
-alias zz='fasd_cd -d -i' # cd with interactive selection
 
-# cd into recent directories ala z
-# TODO: might be redundant with z above...
-zd() {
-    local dir
-    dir="$(fasd -Rdl "$1" | fzf -1 -0 --no-sort +m)" && cd "${dir}" || return 1
-}
-alias j=zd
+antigen bundle jsahlen/tmux-vim-integration.plugin.zsh # open file in vim in adjecent tmux pane
+
+#antigen bundle rupa/z # fuzzy file search
+antigen bundle zsh-users/zsh-completions
+
+# Load the theme.
+antigen theme candy
+
+# Tell Antigen that you're done.
+antigen apply
+
+# TODO: split aliases and settings into their own file
+source $ZSH/plugins/fasd/fasd.plugin.zsh
+FZFZ_RECENT_DIRS_TOOL=fasd # set fasd to be the fuzzy file finder
+
+#Set by fasd by default
+#alias a='fasd -a'        # any
+#alias s='fasd -si'       # show / search / select
+#alias d='fasd -d'        # directory
+#alias f='fasd -f'        # file
+#alias sd='fasd -sid'     # interactive directory selection
+#alias sf='fasd -sif'     # interactive file selection
+#alias z='fasd_cd -d'     # cd, same functionality as j in autojump
+#alias zz='fasd_cd -d -i' # cd with interactive selection
+
 
 # TODO: look into these
 ## View recent f files
@@ -54,21 +63,3 @@ alias j=zd
     #local file
     #file="$(fasd -Rfl "$1" | fzf -1 -0 --no-sort +m)" && dir=$(dirname "$file") && cd "$dir"
 #}
-
-
-antigen bundle aperezdc/zsh-notes # access markdown notes remember to est EDITOR/VISUAL
-#antigen bundle chipsenkbeil/zsh-notes # alternate note manager
-antigen bundle jsahlen/tmux-vim-integration.plugin.zsh # open file in vim in adjecent tmux pane
-
-#antigen bundle rupa/z # fuzzy file search
-antigen bundle zsh-users/zsh-completions
-
-# Load the theme.
-antigen theme candy
-
-
-# Settings
-FZFZ_RECENT_DIRS_TOOL=fasd # set fasd to be the fuzzy file finder
-
-# Tell Antigen that you're done.
-antigen apply
